f74d422c025cf71d49acc3937ae447daf3cb6dfa
anyamehrotra <anyamehrotra07@gmail.com>
2026-06-16 17:48:00 +0530

    add simple calculator project

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a=float(input("enter a number"))
+b=float(input("enter a number"))
+c=input("enter an operator")
+if c=="+":
+    print("addition is ",a+b)
+elif c=="-":
+    print("subtraction is ", a-b)
+elif c=="*":
+        print("multiplication is", a*b)
+elif c=="%":
+    print("remainder is", a%b)
+else:   
+    print("wrong choice  ")    
+           

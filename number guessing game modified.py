95edc7277a806786c3355b0385abdb3956781a73
anyamehrotra <anyamehrotra07@gmail.com>
2026-06-17 11:56:14 +0530

    add number guessing game modified

---
+++ number guessing game modified.py
@@ -0,0 +1,15 @@
+import random
+r=random.randint(1,100)
+
+a=int(input("enter number of times u want this program to run"))
+for i in range(0,a):
+    n=int(input("enter guessed number"))
+    if n>r:
+        print("too high")
+    elif n<r:
+        print("too low")    
+    elif n==r:
+        print("congrats! computer choice was",r) 
+        break
+else:
+        print("oops! you missed it. computers choice was", r)     
